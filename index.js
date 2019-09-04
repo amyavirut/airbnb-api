5f21fa6c92e7677989a06461d751f452895c86ba
Amy Avirut <amyavirut@gmail.com>
2019-09-04 16:02:56 +0700

    080203

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 let app = express()
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Airbnb API')
-})
+app.get('/places', require('./getPlaces'))
+app.get('/', require('./root'))
 
-app.listen(4000, () => {
-	console.log('Ready on port 4000')
+
+app.listen(5000, () => {
+	console.log('Ready on port 5000')
 })
