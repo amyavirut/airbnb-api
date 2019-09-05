ddbc3679fd86c9f181c4fa4ac7bb2659342afc2d
Amy Avirut <amyavirut@gmail.com>
2019-09-05 16:10:13 +0700

    090202

--- models/place.js
+++ models/place.js
@@ -1,15 +1,42 @@
 const mongoose = require('mongoose')
 
 const Place = mongoose.model('place', {
-    title: String,
-    description: String,
-    type: String,
-    city: String,
-    country: String,
-    price: Number,
-    rating: Number,
-    guests: Number,
-    bathrooms: Number
+    title: {
+        type: String,
+        required: [true, 'title is required']
+    },
+    description: {
+        type: String,
+        required: [true, 'description is required']
+    },
+    type: {
+        type: String,
+        required: [true, 'type is required']
+    },
+    city: {
+        type: String,
+        required: [true, 'city is required']
+    },
+    country: {
+        type: String,
+        required: [true, 'country is required']
+    },
+    price: {
+        type: Number,
+        required: [true, 'price is required']
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    guests: {
+        type: Number,
+        required: [true, 'guests is required']
+    },
+    bathrooms: {
+        type: Number,
+        required: [true, 'bathroom is required']
+    },
 })
 
 module.exports = Place 
