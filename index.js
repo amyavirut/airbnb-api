74fb41846a67d11bc99653db60a40df7bc08c97b
Amy Avirut <amyavirut@gmail.com>
2019-09-05 13:17:16 +0700

    090101

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const mongoose =require('mongoose')
+
 let app = express()
 
 app.get('/places', require('./controllers/getPlaces'))
 app.get('/', require('./controllers/root'))
-
+mongoose.connect('mongodb://localhost:27017/airbnb', {useNewUrlParser: true}, (err) => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log('Connected to MongoDB')
+	}
+})
 
 app.listen(5000, () => {
 	console.log('Ready on port 5000')
