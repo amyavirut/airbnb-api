640ec9617954f9a0af89db132472ef21f9bc034f
Amy Avirut <amyavirut@gmail.com>
2019-09-06 16:42:27 +0700

    090701

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.get('/', require('./controllers/root'))
 app.get('/places', require('./controllers/getPlaces'))
 app.post('/places', require('./controllers/postPlaces'))
 app.get('/places/:id', require('./controllers/getPlace'))
+app.patch('/places/:id', require('./controllers/patchPlace'))
 
 app.listen(5000, () => {
 	console.log('Ready on port 5000')
