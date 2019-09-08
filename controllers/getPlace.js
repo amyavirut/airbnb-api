858f94f70ca9f9e13dcac691d5e0bd5ffa2a9ed2
Amy Avirut <amyavirut@gmail.com>
2019-09-08 21:59:22 +0700

    091007

--- controllers/getPlace.js
+++ controllers/getPlace.js
@@ -1,7 +1,7 @@
 const Place = require('../models/place');
 
 module.exports = (req, res) => 
-  Place.findOne({_id:req.params.id}).populate('type')
+  Place.findOne({_id:req.params.id}).populate('type').populate('host','name avatar')
   .then(data => {
     res.send(data)
   }).catch(err => {
