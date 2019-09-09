33ca9f1cf71105af9ce10e51efebdefbdf2d9129
Amy Avirut <amyavirut@gmail.com>
2019-09-09 18:15:54 +0700

    091107

--- controllers/getPlace.js
+++ controllers/getPlace.js
@@ -4,9 +4,12 @@ module.exports = (req, res) =>
   Place.findOne({_id:req.params.id})
   .populate('type')
   .populate('host','name avatar')
-  .populate('amenity')
-  .then(data => {
-    res.send(data)
+  .populate('amenities')
+  .populate('reviews')
+  .then(place => {
+    place.rating = place.reviews.reduce((rating, reviews) => 
+        rating + reviews.rating, 0) / place.reviews.length
+    res.send(place)
   }).catch(err => {
       res.send(err)
   })
--- controllers/getPlaces.js
+++ controllers/getPlaces.js
@@ -11,10 +11,16 @@ module.exports = (req, res) => {
     res.send(data.map(place => {
       place.image = place.images[0]
       delete place.images
+      place.rating = place.reviews.reduce((rating, reviews) => 
+        rating + reviews.rating, 0) / place.reviews.length
       place.reviews = place.reviews.length
+
       return place
     }))
   }).catch(err => {
       res.send(err)
   })
 }
+
+
+
