f9c4673380709c00fe41d142376b77f4723721d5
Amy Avirut <amyavirut@gmail.com>
2019-09-16 15:18:34 +0700

    DB_URL => MONGODB_URI

--- controllers/database.js
+++ controllers/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_URL, {useNewUrlParser: true}, (err) => {
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}, (err) => {
 	if (err) {
 		console.log(err)
 	} else {
