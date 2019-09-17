18ff116b76b773b55701490bfe685165959d436d
Amy Avirut <amyavirut@gmail.com>
2019-09-17 23:03:03 +0700

    120108

--- controllers/postSignup.js
+++ controllers/postSignup.js
@@ -7,6 +7,12 @@ const SALT_ROUNDS = 10
 module.exports = (req, res) => {
     let user = req.body // {name: "Amy", password: "password"}
     user.password = bcrypt.hashSync(user.password, SALT_ROUNDS)
+    User.findOne({email: user.email}).then(exists => {
+        if (exists) {
+            res.status(400)
+            res.send({err: `User with email: ${email} already exists`})
+        }
+    })
     User.create(user).then(data => {
         let token = jwt.sign(data.toJSON(), process.env.JWT_SECRET)
         res.send(token)
