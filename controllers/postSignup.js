c7a69a93f5c2d859a8c0619f0a327c33b9da8783
Amy Avirut <amyavirut@gmail.com>
2019-09-17 22:11:48 +0700

    120105

--- controllers/postSignup.js
+++ controllers/postSignup.js
@@ -1,13 +1,15 @@
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const SALT_ROUNDS = 10
 
 module.exports = (req, res) => {
-    let user = req.body
+    let user = req.body // {name: "Amy", password: "password"}
     user.password = bcrypt.hashSync(user.password, SALT_ROUNDS)
     User.create(user).then(data => {
-        res.send(data)
+        let token = jwt.sign(data, process.env.JWT_SECRET)
+        res.send(token)
     }).catch(err => {
         res.send(err)
     })
